a441508b1dc109b0010bc5b505d0bc5ff9289430
RoyW12 <roywijaya893@gmail.com>
2024-10-19 08:30:00 +0700

    .

--- P8/upload.js
+++ P8/upload.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
+  $("#file").change(function () {
+    if (this.files.length > 0) {
+      $("#upload-button").prop("disabled", false).css("opacity", 1);
+    } else {
+      $("#upload-button").prop("disabled", true).css("opacity", 0.5);
+    }
+  });
+
   $("#upload-form").submit(function (e) {
     e.preventDefault();
 
     var formData = new FormData(this);
-    // revisi
+
     $.ajax({
       type: "POST",
       url: "upload_ajax.php",
